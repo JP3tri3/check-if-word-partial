45a70e1f5d5587d3f1a848cdbc00eea45a20b6b5
JP3tri3 <JP3tri3@gmail.com>
2022-06-14 22:16:37 -0400

    added regex lib type declaration

--- word-regexes/en-regex.d.ts
+++ word-regexes/en-regex.d.ts
@@ -1,3 +1,12 @@
-declare const RegexLib: object;
+interface IRegExLib {
+    4: RegExp;
+    5: RegExp;
+    6: RegExp;
+    7: RegExp;
+    8: RegExp;
+    9: RegExp;
+    10: RegExp;
+}
+declare const RegexLib: IRegExLib;
 export = RegexLib;
 //# sourceMappingURL=en-regex.d.ts.map
